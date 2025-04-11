8b2193dbfee332a56223d2e20685103e187a5119
hanshuhardik <hmhardik5021@gmail.com>
2025-04-11 15:35:43 +0530

    commited by hardik

---
+++ hardik/reverseArray.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void reversee(int arr[], int start, int end)
+{
+	int temp;
+	while (start < end)
+	{
+		temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+
+}	
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	reversee(arr, 0, n-1);
+	printf("Reversed array is \n");
+	for(int i=0;i<n;i++)
+	{
+	    printf("%d ",arr[i]);
+	}
+	return 0;
+}
